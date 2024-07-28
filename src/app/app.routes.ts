488d8e3c8804b1370febb1852d4da2b568cfa46e
Ivica Ivancic <ivica.ivancic007@gmail.com>
2024-07-28 18:57:49 +0200

    rename folder

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,6 @@
 import { Routes } from '@angular/router';
 import { AddEmployeeComponent } from './add-employee/add-employee.component';
-import { UsersComponent } from './users/employees.component';
+import { UsersComponent } from './employees/employees.component';
 
 export const routes: Routes = [
     {path:'', component: UsersComponent},
